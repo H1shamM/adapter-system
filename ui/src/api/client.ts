bb66b887383d799a39ae4d30401cb5c0e087aded
H1sham <hishammorad42@gmail.com>
2026-02-17 20:51:31 +0200

    ui: Add Authentication handling & login

--- ui/src/api/client.ts
+++ ui/src/api/client.ts
@@ -1,5 +1,16 @@
 import axios from "axios"
+import {getToken} from "./token.ts";
 
 export const api = axios.create({
     baseURL: "http://localhost:8000/api/v1",
 })
+
+api.interceptors.request.use((config)=> {
+    const token = getToken()
+
+    if (token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+});
+
