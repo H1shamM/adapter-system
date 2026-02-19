cf239ade9a5ecaf4ae25bb772a195a3dbf3be72a
H1sham <hishammorad42@gmail.com>
2026-02-19 21:40:22 +0200

    ui: refresh token handling

--- ui/src/api/auth.ts
+++ ui/src/api/auth.ts
@@ -1,4 +1,5 @@
 import {api} from "./client.ts";
+import {getToken, setToken} from "./token.ts";
 
 export async function login(username: string, password:string) {
     const params = new URLSearchParams();
@@ -7,5 +8,17 @@ export async function login(username: string, password:string) {
 
     const res = await api.post("/auth/login", params);
 
-    return res;
+    return res.data;
+}
+
+export async function refreshAccessToken() {
+    const refreshToken = await getToken("refresh");
+
+    const res = await api.post("/auth/refresh", {
+        refresh_token: refreshToken,
+    });
+
+    setToken("access", res.data.access_token);
+
+    return res.data.access_token;
 }
